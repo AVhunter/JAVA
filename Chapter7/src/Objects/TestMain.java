597ed03d2f065122b4100a57cfa38ef3d96ba1c9
AVHunter <myshiney@icloud.com>
2017-12-30 23:52:21 +0800

    main方法参数

---
+++ Chapter7/src/Objects/TestMain.java
@@ -0,0 +1,12 @@
+package Objects;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(String i:args) {
+			System.out.println(i);
+		}
+	}
+
+}
