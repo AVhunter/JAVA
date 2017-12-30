c2e528a35aac2f5c4deb7ada3a64abb8b1436997
AVHunter <myshiney@icloud.com>
2017-12-30 23:50:46 +0800

    私有成员变量构造方法

---
+++ Chapter7/src/Objects/Book.java
@@ -0,0 +1,19 @@
+package Objects;
+
+public class Book {
+	private String name;
+	public String getName() {
+		int id=0;
+		setName("Java");
+		return id + this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Book getBook() {
+		return this;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
