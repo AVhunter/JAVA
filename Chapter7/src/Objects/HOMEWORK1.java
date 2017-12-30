a26f327fd9cb89a6c6aa9418e8a8200a499e58e6
AVHunter <myshiney@icloud.com>
2017-12-30 23:51:23 +0800

    作业1

---
+++ Chapter7/src/Objects/HOMEWORK1.java
@@ -0,0 +1,18 @@
+package Objects;
+
+public class HOMEWORK1 {
+	private int x;
+	private int y;
+	public void setx(int i) {
+		this.x=i;
+	}
+	public void sety(int i) {
+		this.y=i;
+	}
+	public int getx() {
+		return x;
+	}
+	public int gety() {
+		return y;
+	}
+}
