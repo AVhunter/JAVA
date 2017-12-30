2e52d528842167ef5f7349d88446790ce07a446c
AVHunter <myshiney@icloud.com>
2017-12-30 23:51:15 +0800

    构造方法

---
+++ Chapter7/src/Objects/CreateObject.java
@@ -0,0 +1,12 @@
+package Objects;
+
+public class CreateObject {
+	public CreateObject() {
+		System.out.println("创建对象");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new CreateObject();
+	}
+
+}
