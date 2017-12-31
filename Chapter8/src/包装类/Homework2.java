4a32569323e3f8025beaaf0583af79e2cdf86be6
AVHunter <myshiney@icloud.com>
2017-12-31 10:34:21 +0800

    作业

---
+++ Chapter8/src/包装类/Homework2.java
@@ -0,0 +1,14 @@
+package 包装类;
+
+public class Homework2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Character c1 = new Character('A');
+		Character c2 =new Character('b');
+		System.out.println(c1+"与"+c2+"相等吗？"+c1.equals(c2));
+		System.out.println(c1+"小写是 "+c1.toLowerCase(c1));
+		System.out.println(c2+"小写是 "+c2.toLowerCase(c2));
+	}
+
+}
