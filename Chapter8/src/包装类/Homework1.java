f865d0e0c9db3e3adaf019aa552cdbe1c760790e
AVHunter <myshiney@icloud.com>
2017-12-31 10:34:13 +0800

    Integer对象转换成int

---
+++ Chapter8/src/包装类/Homework1.java
@@ -0,0 +1,11 @@
+package 包装类;
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer myint = new Integer(4);
+		System.out.println(myint.intValue());
+	}
+
+}
