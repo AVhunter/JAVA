e2a8dc7019bcce532cc5d859a728784252c57468
AVHunter <myshiney@icloud.com>
2017-12-31 10:34:26 +0800

    作业

---
+++ Chapter8/src/包装类/Homework3.java
@@ -0,0 +1,12 @@
+package 包装类;
+
+public class Homework3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boolean b = new Boolean("Helloworld");
+		System.out.println(b);
+		System.out.println(b+"的字符串是 "+b.toString());
+	}
+
+}
