91e03109fbbcce32f6ff9f4efd17b87c6bc12c5f
AVHunter <myshiney@icloud.com>
2017-12-31 10:33:42 +0800

    Integer的最值

---
+++ Chapter8/src/包装类/GetCon.java
@@ -0,0 +1,19 @@
+package 包装类;
+/**
+ * Integer的最值
+ * @author nelson
+ *
+ */
+public class GetCon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int maxint = Integer.MAX_VALUE;
+		int minint = Integer.MIN_VALUE;
+		int intsize = Integer.SIZE;
+		System.out.println("int类型的最大值是:"+maxint);
+		System.out.println("int类型的最小值是:"+minint);
+		System.out.println("int类型的二进制位数是:"+intsize);
+	}
+
+}
