34b22d7297b1cc6015d632dedf13c288c3f3e547
AVHunter <myshiney@icloud.com>
2018-01-01 15:04:13 +0800

    Math类的几个方法--角度弧度正弦余弦

---
+++ Chapter9/src/数字处理类/TrigonometricFunction.java
@@ -0,0 +1,16 @@
+package 数字处理类;
+
+public class TrigonometricFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("90度的正弦值: "+Math.sin(Math.PI/2));
+		System.out.println("0度的余弦值: "+Math.cos(0));
+		System.out.println("tan60 = "+Math.tan(Math.PI/3));
+		System.out.println("2的平方根与2的："+Math.asin(Math.sqrt(2)/2));
+		System.out.println(":"+Math.acos(Math.sqrt(2)/2));
+		System.out.println("120度的弧度值:"+Math.toRadians(120.0));
+		System.out.println("pi/2的角度："+Math.toDegrees(Math.PI/2));
+	}
+
+}
