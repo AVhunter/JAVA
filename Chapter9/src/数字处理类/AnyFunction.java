c43fc67222d7605f77c7d4cb5c84ecd0fadee98a
AVHunter <myshiney@icloud.com>
2018-01-01 15:01:05 +0800

    Math的几个方法

---
+++ Chapter9/src/数字处理类/AnyFunction.java
@@ -0,0 +1,19 @@
+package 数字处理类;
+/**
+ * 两个数比较
+ * Math.Max(a,b)
+ * Math.Min(a,b)
+ * Math.abs(a)
+ * @author nelson
+ *
+ */
+public class AnyFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("4与8之间 大的是："+Math.max(4, 8));
+		System.out.println("4.4与4之间 小的是: "+Math.min(4.4, 4));
+		System.out.println("-3的绝对值是: "+Math.abs(-3));
+	}
+
+}
