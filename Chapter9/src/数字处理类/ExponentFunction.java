76e1de2eb936454f4a0e2912eea85c7906f0eb24
AVHunter <myshiney@icloud.com>
2018-01-01 15:02:21 +0800

    Math类的几个方法

---
+++ Chapter9/src/数字处理类/ExponentFunction.java
@@ -0,0 +1,15 @@
+package 数字处理类;
+
+public class ExponentFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("e的平方值:"+Math.exp(2));
+		System.out.println("ln2="+Math.log(2));
+		System.out.println("lg2="+Math.log10(2));
+		System.out.println("4的平方根="+Math.sqrt(4));
+		System.out.println("8的立方根="+Math.cbrt(8));
+		System.out.println("2的10次方="+Math.pow(2, 10));
+	}
+
+}
