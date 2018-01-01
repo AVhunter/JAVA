ac48ba6ba7cf472093d1eced07d42d03c6e1f0b7
AVHunter <myshiney@icloud.com>
2018-01-01 15:03:04 +0800

    Math的几个方法--舍入

---
+++ Chapter9/src/数字处理类/IntFunction.java
@@ -0,0 +1,15 @@
+package 数字处理类;
+
+public class IntFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("向上取整用ceil 5.2 = "+Math.ceil(5.2));
+		System.out.println("向下取整用floor 5.2 = "+Math.floor(5.2));
+		System.out.println("最接近用rint() 2.7 = "+Math.rint(2.7));
+		System.out.println("最接近用rint() 2.5 = "+Math.rint(2.5));
+		System.out.println("round()是将参数加上0.5后返回最接近的数字: 2.5= "+Math.round(2.5));
+		
+	}
+
+}
