e27511043eba4f91aa5cb012e009e6ca81591059
AVHunter <myshiney@icloud.com>
2018-01-02 13:16:58 +0800

    验证抽象类不能被实例化

---
+++ Chapter10/src/接口继承与多态/Homework1.java
@@ -0,0 +1,14 @@
+package 接口继承与多态;
+/**
+ * 验证抽象类不能被实例化
+ * @author nelson
+ *
+ */
+public abstract class Homework1 {
+	abstract void doit() ;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//new Homework1();
+	}
+
+}
