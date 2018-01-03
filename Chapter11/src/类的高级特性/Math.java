b24fabc3f98b5a66af3d14dc19f81ea256441653
AVHunter <myshiney@icloud.com>
2018-01-03 08:24:48 +0800

    --

---
+++ Chapter11/src/类的高级特性/Math.java
@@ -0,0 +1,9 @@
+package 类的高级特性;
+
+public class Math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("不是java.long.Math类，而是类的高级特性.Math类");
+	}
+}
