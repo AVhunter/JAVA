9bd28da357a1e7ee49494b917f9c715942e0929a
AVHunter <myshiney@icloud.com>
2018-01-03 08:24:01 +0800

    import导入静态成员

---
+++ Chapter11/src/类的高级特性/ImportTest.java
@@ -0,0 +1,16 @@
+package 类的高级特性;
+/**
+ * 使用import导入静态成员
+ * @author nelson
+ *
+ */
+import static java.lang.Math.max;
+import static java.lang.System.out;
+public class ImportTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		out.println("1和4的最大值是"+max(1,4));
+	}
+
+}
