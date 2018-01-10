b9a0a901c65c4efe2daef7ca21901a5ce0739298
AVHunter <myshiney@icloud.com>
2018-01-10 21:37:42 +0800

    Map接口的实现类

---
+++ Chapter14/src/集合类/Emp.java
@@ -0,0 +1,29 @@
+package 集合类;
+/**
+ * Map接口的实现类
+ * @author nelson
+ *
+ */
+
+public class Emp {
+	private String e_id;
+	private String e_name;
+	public Emp(String id,String name) {
+		this.e_id = id;
+		this.e_name=name;
+	}
+	public void setId(String id) {
+		this.e_id=id;
+	}
+	public void setName(String name) {
+		this.e_name = name;
+	}
+	public String getId() {
+		return e_id;
+	}
+	public String getName() {
+		return e_name;
+	}
+	
+
+}
